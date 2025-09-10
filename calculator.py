6c53953e3fdfa32e6ddef57742dd1285b36d5ce1
Ify Ogwu <20157367@tafe.wa.edu.au>
2025-09-10 11:10:19 +0800

    Adding this calculator program for an exercise (i think)

---
+++ calculator.py
@@ -0,0 +1,27 @@
+"""
+This is a chatGPT written simple calculator
+"""
+
+def calculator():
+    print("Simple Calculator")
+    num1 = float(input("Enter first number: "))
+    operator = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "Error: Cannot divide by zero!"
+    else:
+        return "Invalid operator!"
+
+    return f"Result: {result}"
+
+print(calculator())
