b1104be0a29642a5d70a2245df06674122d4e27b
Ify Ogwu <20157367@tafe.wa.edu.au>
2025-09-10 10:56:31 +0800

    Added new cataloguing API

---
+++ catalogue.py
@@ -0,0 +1,25 @@
+"""
+This is a module. It does things related to
+cataloguing.
+"""
+
+def add_item(item):
+    """
+    This function lets you add an item to
+    your collection of things
+
+    Parameters
+    ----------------------------
+    item : the item to add
+    category : the category of stuff 
+                into which to insert the thing
+
+    Returns
+    ----------------------------
+    count: int
+    the number of items in the collection,
+    adter adding the new one
+    
+    Examples
+    """
+    pass
